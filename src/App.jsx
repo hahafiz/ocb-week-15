3c45e933fea1c50f2f59a45b86f6baa85b84b882
hahafiz <hafizqamarul19@gmail.com>
2024-12-25 22:11:04 +0800

    week 15: react basics

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const stailNama = {
+    color: "red",
+    fontSize: 50,
+  };
+
+  return (
+    <div>
+      <p style={stailNama}>Lets</p>
+      <p style={stailNama}>Go To</p>
+      <p style={stailNama}>Japan</p>
+    </div>
+  );
+}
+
+export default App;
